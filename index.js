f472c352ac392e6e04131f0b309286c770f8eeee
mydocker-repo <45119302@qq.com>
2025-11-24 12:25:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   console.log('正在启动 Chromium...');
+
   const browser = await puppeteer.launch({
     headless: 'new',
     executablePath: '/usr/bin/chromium-browser',
@@ -10,18 +11,33 @@ import puppeteer from 'puppeteer';
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
+      '--single-process',
+      '--no-zygote',
+      '--disable-software-rasterizer',  
       '--disable-gpu',
-      '--single-process'
+      '--disable-features=VizDisplayCompositor',
     ],
-    dumpio: true   // 加上这行可以看到 Chromium 的真实日志
+    dumpio: false
   });
 
-  const page = await browser.newPage();
-  await page.goto('https://httpbin.org/html', { waitUntil: 'networkidle2' });
-  
-  await page.screenshot({ path: '/app/screenshot.png' });
-  console.log('截图成功！');
-
-  await browser.close();
-  console.log('浏览器已关闭');
+  try {
+    console.log('浏览器启动成功，准备打开页面...');
+    const page = await browser.newPage();
+    
+    await page.goto('https://time.yingming006.cn/', { 
+      waitUntil: 'domcontentloaded', 
+      timeout: 30000 
+    });
+    
+    console.log('页面加载成功，标题：', await page.title());
+    const dt=new Date().toISOString();
+    await page.screenshot({ path: `/app/success-${dt}.png` });
+    console.log(`截图已保存：success-${dt}.png`);
+    
+  } catch (err) {
+    console.error('出错了：', err.message);
+  } finally {
+    await browser.close();
+    console.log('浏览器已关闭');
+  }
 })();
