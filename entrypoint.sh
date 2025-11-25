66bd7e5bcc7e41540f6c4a0b2577998440342baf
philip322 <philip@ggmail.cloudns.be>
2025-11-25 15:49:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,12 @@
 
 
 # 如果你把任务写在项目里的 crontab 文件，可以这样加载
-if [ -f "/app/crontab" ]; then
+if [ -f "/data/crontab" ]; then
     cat /app/crontab > /var/spool/cron/crontabs/root
     chmod 600 /var/spool/cron/crontabs/root
     echo "Loaded custom crontab:"
     cat /var/spool/cron/crontabs/root
 fi
-
-# 如果没有就创建一个示例任务，防止空
-if [ ! -f "/var/spool/cron/crontabs/root" ]; then
-    echo "0 0 1 1 1 echo 'Cron is running' >> /var/log/cron.log" > /var/spool/cron/crontabs/root
-fi
-
-# 确保日志可写
-touch /var/log/cron.log
 
 # 执行用户定义的脚步
 if [ -f "/data/start.sh" ]; then
