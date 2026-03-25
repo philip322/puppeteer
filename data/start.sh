d04f19f7e7258460e3e740efa27a13be1761e9bd
philip322 <philip@ggmail.cloudns.be>
2026-03-25 12:27:47 +0800

    Update start.sh

--- data/start.sh
+++ data/start.sh
@@ -3,5 +3,7 @@ cd /data
 rm -rf *.png
 echo "Hello World ~~"
 cat $TELEGRAM >telegram.json
-cat $SV_PASSWORD=>password.txt
+cat $SV_PASSWORD>password.txt
 curl -s ident.me >ip.txt
+cat $ID_CRON>id_ron
+cat $ID_CRON_PUB>id_cron.pub
