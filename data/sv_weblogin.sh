d0867ea820fee156ae8b72bbe81564932eeefa93
philip322 <philip@ggmail.cloudns.be>
2026-03-25 12:37:32 +0800

    Update sv_weblogin.sh

--- data/sv_weblogin.sh
+++ data/sv_weblogin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /data
-host=$(curl -s "https://sv.req.xx.kg/$1")
+host=$(curl -s "https://${SV_HOST}/$1")
 echo -n $host > /data/hostinfo.txt
 echo "已更新账号：$host"
 node /data/weblogin.js
